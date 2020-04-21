78f8faae7b4ec1bcdc9c27f84756f9adb134f78e
Atharva Munshi <atharvachaitanya.munshi@sjsu.edu>
2020-04-21 16:35:54 -0700

    Initial commit

---
+++ model/user.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const userScema = mongoose.Schema({
+  name: {
+    type: String,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: 1,
+  },
+  password: {
+    type: String,
+    minlength: 5,
+  },
+  lastname: {
+    type: String,
+    maxlength: 50,
+  },
+  role: {
+    type: Number,
+    default: 0,
+  },
+  token: {
+    type: String,
+  },
+  tokenexp: {
+    type: Number,
+  },
+});
+const User = mongoose.model("User", userScema);
+module.exports = { User };
