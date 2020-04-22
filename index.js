02b76619ffb6906eefef4198b9a0f40dad1dd9f1
Atharva Munshi <atharvachaitanya.munshi@sjsu.edu>
2020-04-21 21:20:03 -0700

    move private info in dev.js

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const bodyparser = require("body-parser");
+const cookieparser = require("cookie-parser");
+const { User } = require("./models/user");
+const config = require("./config/key");
 mongoose
-  .connect(
-    "mongodb+srv://atharva__19:$Atharva2022@react-blog-837hs.mongodb.net/test?retryWrites=true&w=majority?authSource=yourDB&w=1",
-    { useNewUrlParser: true }
-  )
+  .connect(config.mongoURI, { useNewUrlParser: true })
   .then(() => console.log("DB connected"))
   .catch((err) => console.error(err));
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.json({ hello: "hi" });
+});
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
+app.use(cookieparser());
+app.post("/api/users/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userData) => {
+    if (err) return res.status(400).json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 app.listen(5000);
